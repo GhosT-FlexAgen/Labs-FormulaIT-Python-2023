9a8498a90594034158ed05a3bbf53595aee2e0de
GhosT-FlexAgen <106031581+GhosT-FlexAgen@users.noreply.github.com>
2023-10-15 10:49:59 +0300

    Add files via upload

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,6 +1,11 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-middle_player_index = len(list_players) // 2
+# TODO Найдите количество книг, которое можно разместить на дискете
+disk_size = 1.44 * 1024 * 1024
 
-# Разделение участников на две команды
-print(list_players[:middle_player_index])
-print(list_players[middle_player_index:])
+pages = 100
+lines = 50
+chars = 25
+
+book_size = pages * lines * chars * 4
+num_books = int(disk_size // book_size)
+
+print("Количество книг, помещающихся на дискету:", num_books)

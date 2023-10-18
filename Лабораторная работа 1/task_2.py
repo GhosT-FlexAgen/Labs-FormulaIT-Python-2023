c256c00724925b9821b15c1f0e70246cbc024b7a
GhosT-FlexAgen <106031581+GhosT-FlexAgen@users.noreply.github.com>
2023-10-18 14:04:06 +0300

    Исправление ошибок с учетом комментариев
    Task 1 
    - Устранен вспомогательный список numbers_uniq
    
    Task 2
    - Созданы константы для всех числовых значений

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,11 +1,11 @@
-# TODO Найдите количество книг, которое можно разместить на дискете
 disk_size = 1.44 * 1024 * 1024
 
 pages = 100
 lines = 50
 chars = 25
+bytes_in_char = 4
 
-book_size = pages * lines * chars * 4
+book_size = pages * lines * chars * bytes_in_char
 num_books = int(disk_size // book_size)
 
 print("Количество книг, помещающихся на дискету:", num_books)

e7e100bf892dab5e8a6127a1cd8967465753efd8
GhosT-FlexAgen <106031581+GhosT-FlexAgen@users.noreply.github.com>
2023-10-22 13:18:28 +0300

    Улучшение решения задания 1 в соответствии с комментариями
    - sum() заменена на sum() + sum(), чтобы не создавался вспомогательный список перед суммированием
    - подсчёт количества элементов в списке заменен на len(numbers)

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -2,8 +2,8 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 missed_number_id = 4
 
 # Замена значений пропущенного элемента средним арифметическим
-summ_numbers = sum(numbers[:missed_number_id]+numbers[missed_number_id+1:])
-count_numbers = (len(numbers[:missed_number_id]+numbers[missed_number_id+1:]) + 1)
+summ_numbers = sum(numbers[:missed_number_id]) + sum(numbers[missed_number_id + 1:])
+count_numbers = len(numbers)
 
 numbers[missed_number_id] =  summ_numbers / count_numbers
 
